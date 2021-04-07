13aeab15f20c350aace036af8a0277cc167592f8
Shelly-Ling <sontee.ling.chen@gmail.com>
2021-04-07 11:30:18 +0800

    set 404 page and vue router

---
+++ src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main">
+    <Alert show-icon>
+      抱歉，您搜尋的頁面不存在
+      <div class="text">404 Not Found</div>
+
+      <router-link to="/">
+        <div class="home-icon font-size">
+          <Icon type="ios-home"></Icon>
+          <p>回主頁請點</p>
+        </div>
+      </router-link>
+    </Alert>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  text-align: center;
+}
+.home-icon {
+  font-size: 20px;
+  margin: 20px;
+}
+</style>
